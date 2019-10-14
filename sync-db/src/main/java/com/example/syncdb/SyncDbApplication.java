bdadc578469cc38f6065ad7c0ccc839db4d19372
ilagutenkov <il@finch-melrose.com>
2019-10-14 12:56:07 +0300

    add sync and async modules

---
+++ sync-db/src/main/java/com/example/syncdb/SyncDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.syncdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncDbApplication.class, args);
+    }
+
+}
