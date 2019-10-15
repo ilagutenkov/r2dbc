0f2208abd6b9da73ab788b8de98676434239fb71
ilagutenkov <il@finch-melrose.com>
2019-10-16 00:10:02 +0300

    finish test

---
+++ web-test/src/main/java/com/example/webtest/PgStatAll.java
@@ -0,0 +1,18 @@
+package com.example.webtest;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PgStatAll {
+
+//    private Long relid;
+
+    private Long cnt;
+
+}
--- web-test/src/main/java/com/example/webtest/WebTestApplication.java
+++ web-test/src/main/java/com/example/webtest/WebTestApplication.java
@@ -1,13 +1,81 @@
 package com.example.webtest;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
 
 @SpringBootApplication
+@Slf4j
 public class WebTestApplication {
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplateBuilder()
+                .messageConverters(new MappingJackson2HttpMessageConverter(objectMapper()))
+                .build();
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(WebTestApplication.class, args);
+
+
+    }
+
+    @PostConstruct
+    public void init() throws Exception{
+        runTests(false);
+        runTests(false);
+        runTests(false);
+
+        Thread.sleep(5000);
+    }
+
+    private void logThread(Object ... msg){
+
+        log.info("thread: {} object {}", Thread.currentThread(), msg);
+
+    }
+
+    public void runTests(boolean sync){
+        RestTemplate rt=restTemplate();
+
+        String port="8882";
+        if(sync)
+        port="8883";
+
+        String finalPort = port;
+        CompletableFuture.runAsync(()->{
+            Instant start=Instant.now();
+            PgStatAll stat= rt.getForEntity("http://localhost:"+ finalPort +"/sumAge", PgStatAll.class).getBody();
+            Instant end=Instant.now();
+
+            logThread("async","time: "+ (end.toEpochMilli()-start.toEpochMilli()), stat);
+        })
+        .exceptionally(ex->{
+            log.error("problems",ex);
+            return null;
+        });
+
+
     }
 
 }
